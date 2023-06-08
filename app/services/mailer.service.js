d1a0e03e1b32c819dbe9403971d980811953c2a5
Rajat <rajatgaikwad10@gmail.com>
2023-06-08 13:32:02 +0530

    navbar and heading corrections

--- app/services/mailer.service.js
+++ app/services/mailer.service.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "kkmkittu36@gmail.com",
-    pass: "bwypowrjvaszxqdv",
+    pass: "bnbjnjstvzughktd",
   },
 });
 
